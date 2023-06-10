09523e169c99f3d89eb56ca304108a43324b536d
AllifAmey <24247758+AllifAmey@users.noreply.github.com>
2023-06-10 19:59:22 +0100

    fixed vulnerability issue related to vite by upgrading

--- data_analysis_app_frontend/src/components/data projects/Yfinance/YFinance.tsx
+++ data_analysis_app_frontend/src/components/data projects/Yfinance/YFinance.tsx
@@ -1,12 +1,47 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { Button, Flex } from "@chakra-ui/react";
+import useWebSocket, { ReadyState } from "react-use-websocket";
+
 function YFinance(props: any) {
   /*
-  Custom Line Graph using ChartJS
+  Grabbing financial data.
   
   */
+  //Public API that will echo messages sent to it back to the client
+  const [socketUrl, setSocketUrl] = useState(
+    "wss://streamer.finance.yahoo.com/"
+  );
+
+  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket<any>(
+    socketUrl,
+    {
+      share: true,
+      filter: () => false,
+    }
+  );
+  const handleClickSendMessage = useCallback(() => {
+    sendJsonMessage({ subscribe: ["aapl"] });
+  }, []);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: "Connecting",
+    [ReadyState.OPEN]: "Open",
+    [ReadyState.CLOSING]: "Closing",
+    [ReadyState.CLOSED]: "Closed",
+    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
+  }[readyState];
+
   return (
-    <>
-      <div>Empty project</div>
-    </>
+    <Flex justifyContent="space-evenly" alignItems="center" h="100vh">
+      <Button
+        onClick={handleClickSendMessage}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        Send message
+      </Button>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      <ul></ul>
+    </Flex>
   );
 }
 
