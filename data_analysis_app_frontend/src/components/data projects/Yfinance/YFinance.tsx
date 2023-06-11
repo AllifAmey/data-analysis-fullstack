6c0ab92f4464cccfb13e840f598559a3dca9ac31
AllifAmey <24247758+AllifAmey@users.noreply.github.com>
2023-06-11 16:04:27 +0100

    working prototype of real-time cryptocurrency data, now for visualising the data

--- data_analysis_app_frontend/src/components/data projects/Yfinance/YFinance.tsx
+++ data_analysis_app_frontend/src/components/data projects/Yfinance/YFinance.tsx
@@ -1,48 +1,58 @@
-import React, { useState, useCallback, useEffect } from "react";
-import { Button, Flex } from "@chakra-ui/react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
+import React, { useEffect, useState } from "react";
 
-function YFinance(props: any) {
-  /*
-  Grabbing financial data.
-  
-  */
-  //Public API that will echo messages sent to it back to the client
-  const [socketUrl, setSocketUrl] = useState(
-    "wss://streamer.finance.yahoo.com/"
-  );
+const URL_WEB_SOCKET = "wss://stream.binance.com:9443/ws";
+const symbols = ["btc", "eth"];
+const params = symbols.map((val) => {
+  return `${val}usdt@trade`;
+});
+const request = {
+  method: "SUBSCRIBE",
+  params: params,
+  id: 1,
+};
+const CryptoFinance = () => {
+  const [ws, setWs] = useState<WebSocket | null>(null);
+  const [prices, setPrices] = useState({
+    bitCoin: 0,
+    etheruem: 0,
+  });
 
-  const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket<any>(
-    socketUrl,
-    {
-      share: true,
-      filter: () => false,
-    }
-  );
-  const handleClickSendMessage = useCallback(() => {
-    sendJsonMessage({ subscribe: ["aapl"] });
+  useEffect(() => {
+    const wsClient = new WebSocket(URL_WEB_SOCKET);
+    wsClient.onopen = () => {
+      console.log("I am open");
+      setWs(wsClient);
+      wsClient.send(JSON.stringify(request));
+    };
+    wsClient.onclose = () => console.log("ws closed");
+    return () => {
+      wsClient.close();
+    };
   }, []);
 
-  const connectionStatus = {
-    [ReadyState.CONNECTING]: "Connecting",
-    [ReadyState.OPEN]: "Open",
-    [ReadyState.CLOSING]: "Closing",
-    [ReadyState.CLOSED]: "Closed",
-    [ReadyState.UNINSTANTIATED]: "Uninstantiated",
-  }[readyState];
+  useEffect(() => {
+    if (ws) {
+      // here is where something should trigger if a message is recieved.
+
+      ws.onmessage = (evt) => {
+        console.log(evt.data);
+        // parse the data get name and price
+        const data = JSON.parse(evt.data);
+        if (data.s === "ETHUSDT") {
+          setPrices({ ...prices, etheruem: data.p });
+        }
+        if (data.s === "BTCUSDT") {
+          setPrices({ ...prices, bitCoin: data.p });
+        }
+      };
+    }
+  }, [ws, prices]);
 
   return (
-    <Flex justifyContent="space-evenly" alignItems="center" h="100vh">
-      <Button
-        onClick={handleClickSendMessage}
-        disabled={readyState !== ReadyState.OPEN}
-      >
-        Send message
-      </Button>
-      <span>The WebSocket is currently {connectionStatus}</span>
-      <ul></ul>
-    </Flex>
+    <div>
+      {`Bitcoin price is ${prices.bitCoin}`} <br />
+      {`Ethereum price is ${prices.etheruem}`}{" "}
+    </div>
   );
-}
-
-export default YFinance;
+};
+export default CryptoFinance;
