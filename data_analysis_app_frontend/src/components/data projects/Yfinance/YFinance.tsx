4bafc13351967397ed9cca2ffd6a678e6f9dd415
AllifAmey <24247758+AllifAmey@users.noreply.github.com>
2023-06-11 19:42:59 +0100

    improved logic and reduced redundancy adhering to DRY

--- data_analysis_app_frontend/src/components/data projects/Yfinance/YFinance.tsx
+++ data_analysis_app_frontend/src/components/data projects/Yfinance/YFinance.tsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 const URL_WEB_SOCKET = "wss://stream.binance.com:9443/ws";
-const symbols = ["btc", "eth"];
+const symbols = ["btc", "eth", "bnb", "gal", "gmt", "xrp", "doge", "ltc"];
 const params = symbols.map((val) => {
   return `${val}usdt@trade`;
 });
+const intialPricesState = symbols.map((val: string) => {
+  var initial_price = { name: `${val.toUpperCase()}USDT`, price: 0 };
+  return initial_price;
+});
+
 const request = {
   method: "SUBSCRIBE",
   params: params,
@@ -12,15 +17,11 @@ const request = {
 };
 const CryptoFinance = () => {
   const [ws, setWs] = useState<WebSocket | null>(null);
-  const [prices, setPrices] = useState({
-    bitCoin: 0,
-    etheruem: 0,
-  });
+  const [prices, setPrices] = useState(intialPricesState);
 
   useEffect(() => {
     const wsClient = new WebSocket(URL_WEB_SOCKET);
     wsClient.onopen = () => {
-      console.log("I am open");
       setWs(wsClient);
       wsClient.send(JSON.stringify(request));
     };
@@ -33,25 +34,35 @@ const CryptoFinance = () => {
   useEffect(() => {
     if (ws) {
       // here is where something should trigger if a message is recieved.
-
       ws.onmessage = (evt) => {
         console.log(evt.data);
         // parse the data get name and price
         const data = JSON.parse(evt.data);
-        if (data.s === "ETHUSDT") {
-          setPrices({ ...prices, etheruem: data.p });
+        const coin_name = data.s;
+        const new_price_idx = prices.findIndex(
+          (price) => price.name === coin_name
+        );
+        console.log(new_price_idx);
+        console.log(prices);
+        if (new_price_idx === -1) {
+          return;
         }
-        if (data.s === "BTCUSDT") {
-          setPrices({ ...prices, bitCoin: data.p });
-        }
+        var new_price = prices;
+        new_price[new_price_idx].price = parseFloat(data.p);
+        setPrices([...new_price]);
       };
     }
   }, [ws, prices]);
-
+  console.log(prices);
   return (
     <div>
-      {`Bitcoin price is ${prices.bitCoin}`} <br />
-      {`Ethereum price is ${prices.etheruem}`}{" "}
+      {prices.map((coin, index) => {
+        return (
+          <div>{`${coin.name} PRICE: ${
+            coin.price === 0 ? "Waiting for data" : coin.price
+          }`}</div>
+        );
+      })}
     </div>
   );
 };
